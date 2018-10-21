bc3d8aed814403cafde7c3a269f8ca8454148295
Cheche <joseenrique86@gmail.com>
2018-10-21 16:04:21 +0100

    Added Leaflet map integration

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MapContainer from '../container/MapContainer';
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
-        Hello
+        <MapContainer/>
       </div>
     );
   }
